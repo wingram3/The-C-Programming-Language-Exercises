703dde41bf1147bcd90a214db179c907a36363f8
Will Ingram <mingmangram@gmail.com>
2024-05-31 10:39:24 -0700

    6

--- chapter6/exercise6-5.c
+++ chapter6/exercise6-5.c
@@ -1,7 +1,6 @@
 /* Write a function undef that will remove a name and definition
    from the table maintained by lookup and install. */
 
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
